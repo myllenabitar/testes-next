830c0d8a1fcfd9a2c0f1984d702d7fdb61d8524e
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2024-11-27 17:17:15 -0300

    adc perfil

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,24 @@
 export async function GET() {
     const response = await fetch(`https://api.origamid.online/vendas`, {
+        method: 'POST',
         headers: {
-            apikey: 'ORIGAMID123456'
-        }
-    })
-        const vendas = await response.json();
-    return new Response(JSON.stringify(vendas));
+                'Content-Type': 'application/json'
+            
+        }, 
+            body: JSON.stringify({
+            username: 'dog',
+            password:'dog'
+        }),
+    });
+    if(!response.ok){
+        return Response.json({erro: 'Erro na api'});
+    }
+    const data = await response.json();
+
+    cookies().set('token', data.token, {
+        httpOnly: true,
+        secure: true,
+        });
+
+        return Response.json(data);
 }
